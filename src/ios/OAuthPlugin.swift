e140f4ee3334de5f613013dad27a209110591c7f
solidonline <51081224+solidonline@users.noreply.github.com>
2024-01-25 17:04:59 +0100

    callbackScheme

--- src/ios/OAuthPlugin.swift
+++ src/ios/OAuthPlugin.swift
@@ -137,7 +137,7 @@ class OAuthPlugin : CDVPlugin, SFSafariViewControllerDelegate, ASWebAuthenticati
     override func pluginInitialize() {
         let urlScheme = self.commandDelegate.settings["oauthscheme"] as! String
 
-        self.callbackScheme = "\(urlScheme)://oauth_callback"
+        self.callbackScheme = "\(urlScheme)"
         if #available(iOS 10.0, *) {
             self.logger = OSLog(subsystem: urlScheme, category: "Cordova")
         }
